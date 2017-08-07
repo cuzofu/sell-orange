9c7c504623a1289654d9b37a53eda9562f87bdcd
cuzofu <cuzofu@qq.com>
2017-08-07 21:58:59 +0800

    增加state更新日志

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ import todoApp from './components/Reducers/reducer';
 
 let store = createStore(todoApp);
 
+// 每次state更新时，打印日志
+let unSubscribe = store.subscribe(() => {
+    console.log(store.getState())
+});
+
 ReactDOM.render(
     <Provider store={store}>
         <Index />
